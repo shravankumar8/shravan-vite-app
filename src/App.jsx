e4ef57d47b0853f42bd766eaa7f549b92782c5cd
shravan kumar <kumashravan5@gmail.com>
2024-09-10 21:09:50 +0530

    gundu

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React +shravan </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          are gundu ga  {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and dont save this brought 
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click Shravan and play with him again 
       </p>
     </>
   )
